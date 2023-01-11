e9e451fae8b6f51a02b8a59865d1be142917eb88
Eliott-B <eliott.barker2004@gmail.com>
2023-01-11 22:29:25 +0100

    ✅ PEP 8

--- IA/IA_BARKER_OUALI.py
+++ IA/IA_BARKER_OUALI.py
@@ -9,26 +9,37 @@ import random
 FICHIERRECAP = True
 
 
+def ecrire_fichier(data, data2=False):
+    if FICHIERRECAP:
+        if not data2:
+            with open('IA/exit.txt', 'a') as f:
+                f.write(str(data) + '\n')
+        else:
+            with open('IA/infos.txt', 'a') as f:
+                f.write(str(data) + '\n')
+
+
 class IA_Diamant:
-    
-    def __init__(self, match : str): # Ne pas changer les paramètres
+
+    def __init__(self, match: str):  # Ne pas changer les paramètres
         """génère l'objet de la classe IA_Diamant
 
         Args:
             match (str): decriptif de la partie
         """
-        # self.ecrire_fichier('\n'+match)
+        # ecrire_fichier('\n'+match)
         with open('IA/exit.txt', 'w') as f:
-                f.write('\n'+match+'\n')
+            f.write('\n'+match+'\n')
         self.match = match
         self.coffre = 0  # nombre de diamants que l'IA à sécurisé
         self.diamant_tmp = 0  # nombre de diamants temporaires que l'IA a récupéré pendant l'exploration
-        self.reliques = [] # Liste des reliques récupérées
-        self.manche = 0 # Manche actuelle
-        self.piegesorties = [] # Liste des pièges sortis
-        self.reliqueattente = [] # Liste des reliques en attente
-        self.diamantattente = 0 # Nombre de diamants sur le tapis
-        # Dictionnaire des statistiques des joueurs (nombre de diamants sécurisés [int], reliques [list], nombre de diamants temporaires [int])
+        self.reliques = []  # Liste des reliques récupérées
+        self.manche = 0  # Manche actuelle
+        self.piegesorties = []  # Liste des pièges sortis
+        self.reliqueattente = []  # Liste des reliques en attente
+        self.diamantattente = 0  # Nombre de diamants sur le tapis
+        # Dictionnaire des statistiques des joueurs
+        # (nombre de diamants sécurisés [int], reliques [list], nombre de diamants temporaires [int])
         self.statsjoueurs = self.generation_stat_joueur()
         self.cartesrestantes = {
             1: 1,
@@ -40,7 +51,7 @@ class IA_Diamant:
             9: 1,
             11: 2,
             13: 1,
-            14: 1, 
+            14: 1,
             15: 1,
             17: 1,
             'P1': 3,
@@ -50,9 +61,9 @@ class IA_Diamant:
             'P5': 3,
             'R5': 3,
             'R10': 2
-        } # dictionnaires des cartes restantes (carte : nombre de cartes)
+        }  # dictionnaires des cartes restantes (carte : nombre de cartes)
 
-    def info_manche_IA(self, tour : str):
+    def info_manche_ia(self, tour: str):
         """Appelé à chaque action afin de récupérer les informations concernant notre IA.
         Permet d'enregistrer les informations dans les variables comme par exemple
         les diamants temporaire et relique de notre IA.
@@ -70,7 +81,7 @@ class IA_Diamant:
                 self.reliqueattente.append(5)
             else:
                 self.reliqueattente.append(10)
-        
+
         if choix == 'X':  # Vérifie que l'IA reste 
             # Compte le nombre d'IA explorant
             nombre_explorant = 0
@@ -83,21 +94,21 @@ class IA_Diamant:
             if info[1] in [str(i) for i in range(20)]:
                 carte = int(info[1])
                 self.diamant_tmp += (carte//nombre_explorant)
-                self.ecrire_fichier('POCHES : '+str(self.diamant_tmp))
+                ecrire_fichier('POCHES : '+str(self.diamant_tmp))
         elif choix == 'R':
             entrant = list()
             i = 0
             for ia in ia_choix:
-                    if ia == 'R':
-                        entrant.append(i)
-                    i += 1
+                if ia == 'R':
+                    entrant.append(i)
+                i += 1
             if len(entrant) == 1:
                 if len(self.reliqueattente) > 0:
                     # Ajoute la relique dans les poches de l'IA
                     self.reliques.append(self.reliqueattente[0])
                     self.cartesrestantes['R'+str(self.reliqueattente[0])] -= 1
-            
-    def info_joueurs(self, tour : str):
+
+    def info_joueurs(self, tour: str):
         """Appelé à chaque action afin de récupérer les informations concernant les autres joueurs.
         Permet d'enregistrer les informations dans les variables comme par exemple
         les diamants sécurisés et reliques des autres joueurs.
@@ -119,7 +130,7 @@ class IA_Diamant:
         else:
             return 'R'
 
-    def action(self, tour : str) -> str: # Ne pas changer les paramètres
+    def action(self, tour: str) -> str:  # Ne pas changer les paramètres
         """Appelé à chaque décision du joueur IA
 
         Args:
@@ -128,16 +139,14 @@ class IA_Diamant:
         Returns:
             str: 'X' ou 'R'
         """
-        self.info_joueurs(tour) # Appel la fonction pour actualiser les informations récupérés du tour.
-        self.info_manche_IA(tour) # Appel la fonction pour actualiser les informations récupérés du tour.
-        
-        self.ecrire_fichier(tour)
-        
-        
+        self.info_joueurs(tour)  # Appel la fonction pour actualiser les informations récupérés du tour.
+        self.info_manche_ia(tour)  # Appel la fonction pour actualiser les informations récupérés du tour.
+
+        ecrire_fichier(tour)
 
         return self.lebonchoix()
 
-    def fin_de_manche(self, raison : str, dernier_tour : str) -> None: # Ne pas changer les paramètres
+    def fin_de_manche(self, raison: str, dernier_tour: str) -> None:  # Ne pas changer les paramètres
         """Appelé à chaque fin de manche
 
         Args:
@@ -150,32 +159,32 @@ class IA_Diamant:
         # Retire les cartes pièges qui sont sortis
         if raison != 'R':
             self.cartesrestantes[raison] -= 1
-        
+
         derniere_action = dernier_tour.split('|')[0].split(',')
-        for i in range(1,int(self.match.split('|')[1])):
+        for i in range(1, int(self.match.split('|')[1])):
             if derniere_action[i] != 'X':
                 self.statsjoueurs[i][0] = self.statsjoueurs[i][2]
-        
-        self.ecrire_fichier(dernier_tour)
-        self.ecrire_fichier(raison)
-        self.ecrire_fichier('POCHES : '+str(self.diamant_tmp))
-        self.ecrire_fichier('COFFRE : '+str(self.coffre))
-        self.ecrire_fichier('FIN DE MANCHE')
+
+        ecrire_fichier(dernier_tour)
+        ecrire_fichier(raison)
+        ecrire_fichier('POCHES : '+str(self.diamant_tmp))
+        ecrire_fichier('COFFRE : '+str(self.coffre))
+        ecrire_fichier('FIN DE MANCHE')
         # Remet les diamants temporaires à 0
         self.diamant_tmp = 0
         self.manche += 1
 
         # Remet à 0 les diamants temporaires des joueurs
-        for i in range(1,int(self.match.split('|')[1])):
+        for i in range(1, int(self.match.split('|')[1])):
             self.statsjoueurs[i][2] = 0
 
-        self.ecrire_fichier(self.manche)
-        self.ecrire_fichier('STATS JOUEURS'+str(self.statsjoueurs), True)
-        self.ecrire_fichier('CARTES RESTANTES'+str(self.cartesrestantes), True)
-        self.ecrire_fichier('PIEGES SORTIS'+str(self.piegesorties), True)
-        self.ecrire_fichier("------------------", True)
+        ecrire_fichier(self.manche)
+        ecrire_fichier('STATS JOUEURS'+str(self.statsjoueurs), True)
+        ecrire_fichier('CARTES RESTANTES'+str(self.cartesrestantes), True)
+        ecrire_fichier('PIEGES SORTIS'+str(self.piegesorties), True)
+        ecrire_fichier("------------------", True)
 
-    def game_over(self, scores : str) -> None: # Ne pas changer les paramètres
+    def game_over(self, scores: str) -> None:  # Ne pas changer les paramètres
         """Appelé à la fin du jeu ; sert à ce que vous voulez
 
         Args:
@@ -184,18 +193,9 @@ class IA_Diamant:
         with open('IA/exit.txt', 'a') as f:
             f.write('END\n'+scores)
 
-    def ecrire_fichier(self, data, data2=False):
-        if FICHIERRECAP:
-            if data2 == False:
-                with open('IA/exit.txt', 'a') as f:
-                    f.write(str(data)+'\n')
-            else:
-                with open('IA/infos.txt', 'a') as f:
-                    f.write(str(data)+'\n')
-
     def generation_stat_joueur(self):
         stat = {}
         nb_joueur = int(self.match.split('|')[1])
         for i in range(nb_joueur):
-            stat[i] = [0,[],0]
+            stat[i] = [0, [], 0]
         return stat
